3b820c0dfd71466ec20d6a9e8d61306e6455c208
marioreg <marioreg@hotmail.com>
2018-05-13 17:40:48 -0500

    final bug pending

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,26 @@
 
 //Define variables
+var userMatch = 0;
+var wins = 0;
+var losses = 0;
 
-var wins =0;
-var losses =0;
+
+
 var red = 0;
 var blue = 0;
 var yellow = 0;;
 var purple = 0;
 var matchNumber=0;
-var userMatch = 0;
-$('#wins').text("Wins: " + wins);
-$('#losses').text("losses: " +losses);
+
+var temp =0;
+
 
 $(document).ready(function(){
 
 
     numberToMatch();
     newGame();
+    
    
 
 
@@ -32,78 +36,121 @@ function numberToMatch(){
     
    
     function newGame(){
+        $('#wins').text("Wins: " + wins);
+        $('#losses').text("losses: " + losses);
+
         userMatch = 0;
         $("#total-score").text(userMatch);
 
-        //function that generates a random number when user clicks on the red crystal and saves it to a variable
-        //it also adds the vaule to the current score
+      //function that generates a random number when user clicks on the red crystal and saves it to a variable
+      //it also adds the vaule to the current score
         $("#crystal1").click(function redCrystal(){
-        red = Math.ceil(Math.random()*12);
+            
+        temp = Math.ceil(Math.random()*12);
+        if (red === 0){
+            red = temp;
+            userMatch = userMatch + red;
+            $("#total-score").text(userMatch);
+             console.log("The red crystal has a vaule of " + red);
+
+        } else {
         userMatch = userMatch + red;
         $("#total-score").text(userMatch);
-         console.log("The red crystal has a vaule of " + red);
-        
+         console.log("The red crystals has a vaule of " + red);
+        }
+        winLose();
         });
+        
     //function that generates a random number when user clicks on the blue crystal and saves it to a variable
     //it also adds the vaule to the current score
 
-    $("#crystal2").click(function blueCrystal(){
-        blue = Math.ceil(Math.random()*12);
+        $("#crystal2").click(function blueCrystal(){
+            
+        temp = Math.ceil(Math.random()*12);
+        if (blue === 0){
+            blue = temp;
+            userMatch = userMatch + blue;
+            $("#total-score").text(userMatch);
+            console.log("The blue crystal has a vaule of " + blue);
+
+        } else {
         userMatch = userMatch + blue;
         $("#total-score").text(userMatch);
-        console.log("The blue crystal has a vaule of " + blue);
+        console.log("The blue crystals has a vaule of " + blue);
+        }
+        winLose();
         });
-
+        
     //function that generates a random number when user clicks on the yellow crystal and saves it to a variable
     //it also adds the vaule to the current score
 
-    $("#crystal3").click(function yellowCrystal(){
-        yellow = Math.ceil(Math.random()*12);
+        $("#crystal3").click(function yellowCrystal(){
+            
+        temp = Math.ceil(Math.random()*12);
+        if (yellow === 0) {
+            yellow= temp;
+            userMatch = userMatch + yellow;
+            $("#total-score").text(userMatch);
+            console.log("The yellow crystal has a vaule of " + yellow);
+        } else {
         userMatch = userMatch + yellow;
         $("#total-score").text(userMatch);
-        console.log("The yellow crystal has a vaule of " + yellow);
+        console.log("The yellow crystals has a vaule of " + yellow);
+        }
+        winLose();
         });
-
+        
     //function that generates a random number when user clicks on the purple crystal and saves it to a variable
     //it also adds the vaule to the current score
-    $("#crystal4").click(function purpleCrystal(){
+        $("#crystal4").click(function purpleCrystal(){
        
-        purple = Math.ceil(Math.random()*12);
+        temp = Math.ceil(Math.random()*12);
+        if (purple=== 0){
+        purple = temp;
         userMatch = userMatch + purple;
         $("#total-score").text(userMatch);
-        console.log("The purple crystal has a vaule of " + purple);
-        
+        console.log("The purple crystals has a vaule of " + purple);
+
+        } else {
+        userMatch = userMatch + purple;
+        $("#total-score").text(userMatch);
+        console.log("The purple crystals has a vaule of " + purple);
+        }
+        winLose();
         });
-
- 
-
-  
+        
+        
     }
 
+    function winLose(){
 
-    
+        if (userMatch === matchNumber){
 
+            alert("You win!");
+            wins++;
+            temp = 0;
+            red = 0;
+            blue = 0;
+            yellow = 0;
+            purple = 0;
+            numberToMatch();
+            newGame();
+        } 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if (userMatch > matchNumber) {
+            alert("You lose!");
+            losses++;            
+            temp = 0;
+            red = 0;
+            blue = 0;
+            yellow = 0;
+            purple = 0;
+            numberToMatch();
+            newGame();
+            
+        }
+        
+    }
 
 
 });
